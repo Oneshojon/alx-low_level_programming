de6f97e0dc082a993b4fdabe4e446cdb97dd6ec3
Titus Denis <deetitos@gmail.com>
2023-07-14 17:31:17 +0100

    committing the alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - This function will jump some alphabets 
+ *
+ *Return: It will return small letters
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
