e7516617873ebed482e2ca38ff62fecd2267ea99
Titus Denis <deetitos@gmail.com>
2023-07-14 14:49:41 +0100

    Commit for the alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- *2-print_alphabet.c - A function to print lowercase letters
+ *main - A function to print lowercase letters
  *
  *Return: The return values are [a-z]
  */
 int main(void)
 {
 	char letter = 'a';
-	while (letter<='z')
+
+	while (letter <= 'z')
 	{
 		putchar(letter);
 		letter++;
