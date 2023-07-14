20038086b61d9d0e154f16b7e7055fab30f1a77b
Titus Denis <deetitos@gmail.com>
2023-07-14 11:17:14 +0100

    committed for positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point for the program
+ *n: The nuber to be checked
+ *
+ *Return: This will return is_positive, or is_negetive
+ */
+int main(void)
+{
+	int n;
+	if (n<0)
+		printf("is negative\n");
+	else if (n==0)
+		printf("is zero\n");
+	else
+		print("is positive\n");
+return (0);
+}
