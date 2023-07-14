c3b43ca2ec935b9fbda080d221e7b342326545cf
Titus Denis <deetitos@gmail.com>
2023-07-14 01:15:44 +0100

    The printf function added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ *my_func - This function prints this 
+ *@arg1: It comes with just one 
+ *
+ *value: This will return the message
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (o);
+}
+
