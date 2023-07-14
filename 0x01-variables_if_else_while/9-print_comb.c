2d5b95df2ab0b2d6c72de6ac271005c15fcb11ea
Titus Denis <deetitos@gmail.com>
2023-07-14 23:25:38 +0100

    committing for numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - This will print all numbers
+ *
+ *Return: The numers0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
+ */
+int main(void)
+{
+	int val = '0';
+
+	while (val <= '9')
+	{
+		putchar(val);
+		putchar(',');
+		putchar(' ');
+		val++;
+	}
+	putchar('\n');
+	return (0);
+}
