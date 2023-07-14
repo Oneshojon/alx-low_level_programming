b601aed901e6f7d71f3ab82845bcbbe15776a5a9
Titus Denis <deetitos@gmail.com>
2023-07-14 18:26:00 +0100

    committing the numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/**
+ *main - will print values using putchar
+ *
+ *Return: This will return all values
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar('0' + number);
+		number++;
+	}
+	putchar("\n");
+	return (0);
+}
