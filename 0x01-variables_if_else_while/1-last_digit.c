a41be1169516d959fb938a723477b80e9242ab5b
Titus Denis <deetitos@gmail.com>
2023-07-14 13:09:04 +0100

    committing after brief correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  *1-last_digit.c - will evaluate the modulo and check for negative positive or 0
@@ -8,10 +10,15 @@
 int main(void)
 {
 	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	int last_digit = n % 10;
-	if (last_digit>5)
+
+	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if (last_digit==0)
+	else if (last_digit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else
 		printf("Last digit of %d is %d and is less than 6\n", n, last_digit);
