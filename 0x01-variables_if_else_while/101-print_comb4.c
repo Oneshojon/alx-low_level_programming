d6fc0d97784cf1c6f3d832be915b8aefffde1276
Titus Denis <deetitos@gmail.com>
2023-07-15 02:37:31 +0100

    commited the threes

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *main - This will print in three
+ *
+ *Return: The return value vill be unique
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i < '8'; i++)
+	{
+		for (j = '1'; j < '9'; j++)
+		{
+			for (k = '2'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == '7' && j == '8' && k == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
