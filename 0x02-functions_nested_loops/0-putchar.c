17f24dda79ab5705499ee53a783e2bd30e951b46
Titus Denis <deetitos@gmail.com>
2023-07-18 18:26:15 +0100

    Commiting the first excercise

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <main.h>
+#include "main.h"
 
 /**
  *main - This function prints _putchar
  *
  *Return: Returns o success
  */
-int main()
+int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
