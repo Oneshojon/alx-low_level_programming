21a7492bef66c48fcb77267a68ca36075a497cc5
Titus Denis <deetitos@gmail.com>
2023-07-19 03:31:29 +0100

    Timer commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *jack_bauer - returns time
+ *
+ *Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
