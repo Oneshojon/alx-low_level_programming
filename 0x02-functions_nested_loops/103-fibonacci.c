1f585f7f4b3d2e56538a6358662081bc7dff9b3c
Titus Denis <deetitos@gmail.com>
2023-07-19 23:04:59 +0100

    committed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,21 +10,16 @@ int main(void)
 	int i = 0;
 	long j = 0, k = 1, f, sum;
 
-	while (i < 50)
+	while (i < 50 && f <= 4000000)
 	{
 		f = j + k;
-		if ((f % 2) == 0 && f <= 4000000)
+		if ((f % 2) == 0)
 		{
 			sum += f;
 		}
-		if (f <= 4000000)
-		{
-			j = k;
-			k = f;
-			i++;
-		}
-		else
-			break;
+		j = k;
+		k = f;
+		i++;
 	}
 	printf("%ld\n", sum);
 	return (0);
