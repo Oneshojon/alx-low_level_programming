f4522244169c83421bb9677b82370b92de6e6aa0
Titus Denis <deetitos@gmail.com>
2023-07-19 17:58:02 +0100

    101-natural.c added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - number
+ *
+ *Return: Zero always
+ */
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i > 2 && i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+			i++;
+		}
+	}
+	printf("%d\n" sum);
+	return (0);
+}
