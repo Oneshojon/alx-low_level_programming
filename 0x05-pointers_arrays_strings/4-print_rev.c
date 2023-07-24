c3bbb3b705ccbeedb53b67e85a9c747b7985690c
Titus Denis <deetitos@gmail.com>
2023-07-25 00:18:00 +0100

    comitted print_reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_rev - Prints the reverse of the provided str
+ *@s: Params
+ *
+ *Returns: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int count = 0;
+	int x;
+
+
+	while (s[i] != '\0')
+	{
+		count += 1;
+		i++;
+	}
+
+	for (x = count; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
