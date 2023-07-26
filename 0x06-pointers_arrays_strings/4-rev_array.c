d3d9f2ac8fe42c27457a75c7231a69208c02db98
Titus Denis <deetitos@gmail.com>
2023-07-26 23:40:39 +0100

    committed reverser

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses the values of an array
+ *@a: Pointer to the array
+ *@n: number of elements in the array
+ *
+ *Return: Buffer of the reversed array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int temp;
+
+	while (i < j / 2)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j--] = temp;
+		i++;
+	}
+}
