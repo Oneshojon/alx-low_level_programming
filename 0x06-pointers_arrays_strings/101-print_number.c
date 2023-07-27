eb4a67f7e6d31c53e3dce934bcf5f28f3ed96bf4
Titus Denis <deetitos@gmail.com>
2023-07-28 00:24:56 +0100

    Commited

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_number - prints numbers in a string
+ *@n: string of ASCII characters
+ *
+ *Return: Nothing
+ */
+void print_number(int n)
+{
+	int i = 10;
+	int digit;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n < 10)
+		_putchar(n + '0');
+	else
+	{
+		while (n / i >= 10)
+		{
+			i *= 10;
+		}
+		while (n != 0)
+		
+		{
+			digit = n / i;
+			_putchar(digit + '0');
+			n %= i;
+			i /= 10;
+		}
+	}
+}
