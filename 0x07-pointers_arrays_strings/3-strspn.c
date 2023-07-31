69d14b78e4976108452465a3d0d142b435027136
Titus Denis <deetitos@gmail.com>
2023-07-31 19:37:32 +0100

    comitted the byte counter

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strspn - Returns the nuber of bytes in a sub-string
+ *@s: The full string
+ *@accept: stop point
+ *
+ *Return: A count of chracters
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == *accept)
+		{
+			return (i + 1);
+		}
+		i++;
+		s++;
+	}
+	return (i);
+}
