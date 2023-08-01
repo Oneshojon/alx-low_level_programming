9c963da3024731586345245bc3a2458934e651eb
Titus Denis <deetitos@gmail.com>
2023-08-01 02:24:45 +0100

    committed

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *set_string - Sets the value os a pointer to a character
+ *@s: character
+ *@to: pointer
+ *
+ *Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	if (s != NULL)
+
+	{
+		size_t len = (to != NULL) ? strlen(to) : 0;
+
+
+		char *new_str = (char *)malloc((len + 1) * sizeof(char));
+
+		if (new_str != NULL)
+		{
+			if (len > 0)
+			{
+				strcpy(new_str, to);
+			}
+
+			*s = new_str;
+		}
+	}
+}
