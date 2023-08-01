dfe38c622f1e63d0430cf55fbdb4263c432628f2
Titus Denis <deetitos@gmail.com>
2023-08-01 01:36:27 +0100

    Committed

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_chessboard - Returns a chessboard
+ *@a: y
+ *
+ *Return: The image
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
