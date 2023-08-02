a023fafc803d738d1e0825a5dc7ddd1306a64e15
Titus Denis <deetitos@gmail.com>
2023-08-02 21:07:11 +0100

    committed

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *checker - checks a number, returns 1 if n is prime else returns -1
+ *@n: The number to be checked
+ *@x: number to increment while checking
+ *
+ *Return: 1 or -1
+ */
+int checker(int x, int n)
+{
+	if (n < 2 || (n % x == 0 && x < n))
+	{
+		return (0);
+	}
+	if (x * x < n)
+		return checker(x + 1, n);
+	return (1);
+}
+
+/**
+ *is_prime_number - Checks n to see if it is a prime numer
+ *@n: The number to be checked
+ *
+ *Return: 1 if n is prime, else return -1
+ */
+int is_prime_number(int n)
+{
+	return (checker(2, n));
+}
