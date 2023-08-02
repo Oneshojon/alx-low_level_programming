ca308e4ccc4b4693474aa152ab94c959494bc844
Titus Denis <deetitos@gmail.com>
2023-08-02 15:52:40 +0100

    committed

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+  nclude "main.h"
+
+/**
+ *_puts_recursion - prints a string
+ *@s: The string to be printed
+ *
+ *Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+}
