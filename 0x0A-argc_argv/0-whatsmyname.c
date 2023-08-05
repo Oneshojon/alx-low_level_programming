27833eb1a381363d0f692c2046214dbd1b2d674a
Titus Denis <deetitos@gmail.com>
2023-08-05 02:16:01 +0100

    Committed0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints the name of the function followed by a line
+ *@argc: Number of arguments in the program
+ *@argv: An array of pointers to the arguments
+ *
+ *Return: Nothing
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
