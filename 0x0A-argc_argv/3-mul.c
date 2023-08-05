eed3f663364e6e007b23ccbc143d044b1745a4c3
Titus Denis <deetitos@gmail.com>
2023-08-05 03:29:27 +0100

    Multiplier added

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Will return the product of two numbers
+ *@argc: count of arguments
+ *@argv: pointers to arguments
+ *
+ *Return 1 for error 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi (argv[1]);
+	j = atoi (argv[2]);
+	prod = i * j;
+	printf("%d\n", prod);
+	return (0);
+}
