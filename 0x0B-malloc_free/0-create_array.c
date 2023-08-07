f3d974af98a4ade963b0e48b56180ae431337450
Titus Denis <deetitos@gmail.com>
2023-08-07 19:29:08 +0100

    0-create_array.c committed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ *create_array - Creates an array of char's and initialize it with c
+ *@size: The size of the characters
+ *@c: The character to initialize with
+ *
+ *Return: A pointer to the array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+	arr = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory will contain projects on 0x0B-malloc_free
