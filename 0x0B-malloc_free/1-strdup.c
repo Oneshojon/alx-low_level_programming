b6981d50b4afb732f8116107fa200f82dd6205a2
Titus Denis <deetitos@gmail.com>
2023-08-08 09:50:00 +0100

    Commited 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - Duplicates a str
+ *@str: The string to be duplicated
+ *
+ *Return: A pointer to the new string
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned long int i = 0;
+	int j;
+
+	if (str == NULL)
+		return (NULL);
+	{
+		i++;
+	}
+	arr = malloc(sizeof(char) * i + 1);
+	if (arr == NULL)
+		return (NULL);
+	for (j = 0; str[j]; j++)
+	{
+		arr[j] = str[j];
+	}
+	arr[j] = '\0';
+	return (arr);
+}
