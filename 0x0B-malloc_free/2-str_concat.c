99df02ad97c530d8bc569e9bfd888540ca43c352
Titus Denis <deetitos@gmail.com>
2023-08-08 17:30:20 +0100

    2-str_concat.c commited

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,10 @@ char *str_concat(char *s1, char *s2)
 	int k = 0;
 	int m = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
@@ -26,13 +30,13 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	while (k < i)
 	{
-		arr[k] = (s1[k] ==  NULL) ? '' : s1[k];
+		arr[k] = s1[k];
 		k++;
 	}
 
 	while (k < (i + j) && m < j)
 	{
-		arr[k] = (s2[m] ==  NULL) ? '' : s2[m];
+		arr[k] = s2[m];
 		m++;
 		k++;
 	}
