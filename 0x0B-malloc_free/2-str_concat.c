334402141327f0a59dc8a92d02cc93951ef79689
Titus Denis <deetitos@gmail.com>
2023-08-08 15:54:24 +0100

    commited 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *str_concat - concats s1 to s2 in a new array
+ *@s1: first string
+ *@s2: Second string
+ *
+ *Return: A pointer to the combined string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int m = 0;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	arr = malloc((i + j) * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+	while (k < i)
+	{
+		arr[k] = s1[k];
+		k++;
+	}
+	while (k < (i + j) && m < j)
+	{
+		arr[k] = s2[m];
+		m++;
+		k++;
+	}
+	arr[k] = '\0';
+	return (arr);
+}
