7c1d17f0aab12c265675b3694a738f0b5b9de493
Titus Denis <deetitos@gmail.com>
2023-08-09 04:00:05 +0100

    commited

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *free_grid - free up a 2D grid
+ *@grid: pointer to 2d
+ *@height: Height of grid
+ *
+ *Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid);
+	free(grid);
+}
