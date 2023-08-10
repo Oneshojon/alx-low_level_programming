b440eccab3b5798d9dd0173aff56672f639e83bf
Titus Denis <deetitos@gmail.com>
2023-08-10 18:50:58 +0100

    returned the solution for 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *_calloc - Allocates memory for an arry
+ *@nmemb: length of the array
+ *@size: size in bytes of each member of the array
+ *
+ *Return: Pointer to the allocated memory or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	return (arr);
+}
