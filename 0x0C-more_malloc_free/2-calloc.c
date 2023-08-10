0b4f4c7d5f407d98615d555a546fc6e6c53f3c17
Titus Denis <deetitos@gmail.com>
2023-08-10 21:20:48 +0100

    Commited 2-calloc.c again

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	int count = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	for (count = 0; count < nmemb; count++)
+		arr[count] = 0;
 	return (arr);
 }
