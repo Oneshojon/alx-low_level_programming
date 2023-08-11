b8d3016c52667b3b39103247c3dea3ce09e385d5
Titus Denis <deetitos@gmail.com>
2023-08-11 17:50:02 +0100

    commited

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
 /**
  *is_positive_num - Checks for positive num
@@ -27,14 +29,14 @@ int is_positive_num(const char *str)
 
 
 
-int main(int argc, char argv)
+int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\m");
+		printf("Error\n");
 		return (98);
 	}
-	if (!is_positive_num(argv[1]) || !s_positive_num(argv[2]))
+	if (!is_positive_num(argv[1]) || !is_positive_num(argv[2]))
 	{
 		printf("Error\n");
 		return (98);
@@ -42,6 +44,6 @@ int main(int argc, char argv)
 	unsigned long long num1 = strtoull(argv[1], NULL, 10);
 	unsigned long long num2 = strtoull(argv[2], NULL, 10);
 
-	printf("%llu\", num1 * num2);
+	printf("%llu\n", num1 * num2);
 	return (0);
 }
