846cb08290066551bcfe3ec8a11f767143a652bf
Titus Denis <deetitos@gmail.com>
2023-08-14 23:18:18 +0100

    2-main.ccommited

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
-#include <stdio.h>>
+#include <stdio.h>
 
 /**
  *main - Prints the name of the file it was compliled from
  *
  *Return: Nothing
  */
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
