932c348559eca47851840bb73b217fbc9bedcff8
Titus Denis <deetitos@gmail.com>
2023-08-15 16:10:11 +0100

    2-print_dog.c added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 	{
 		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nill)\n"));
 		d->age ? (printf("Age: %f\n", d->age)) : (printf("Age; (nill)\n"));
-		d->owner : (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,5 @@ struct dog
 /*Additional functions*/
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
