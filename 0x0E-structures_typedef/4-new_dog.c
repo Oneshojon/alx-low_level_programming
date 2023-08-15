91e5a20f4934833aab83c1b43036f4dfbd720eff
Titus Denis <deetitos@gmail.com>
2023-08-15 19:05:18 +0100

    commited 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *new_dog - Creates a new dog
+ *@name: Name
+ *@age: Age
+ *@owner: owner
+ *
+ *Return: Success
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+{
+	dog_t *newDog = malloc(sizeof(dog_t));
+
+	if (newDog)
+	{
+		newDog.name = strdup(name);
+		newDog.age = age;
+		newDog.owner = strdup(owner);
+		if (!newDog.name || !newDog.owner)
+		{
+			free(newDog.name);
+			free(newDog.owner);
+			free(newDog);
+			return (NULL);
+		}
+	}
+	return (newDog);
+}
