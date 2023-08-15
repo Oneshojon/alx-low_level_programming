cf0dd9e08b38fdcb46ceb1478b1c9efb9be0aaf8
Titus Denis <deetitos@gmail.com>
2023-08-15 16:21:58 +0100

    2-print_dog.c commited

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nill)\n"));
-		d->age ? (printf("Age: %f\n", d->age)) : (printf("Age; (nill)\n"));
-		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
