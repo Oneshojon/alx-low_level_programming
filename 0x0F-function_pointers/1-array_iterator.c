9ae01f82d13298df018276719e0bd3aec4309497
Titus Denis <deetitos@gmail.com>
2023-08-17 01:34:41 +0100

    commited

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "functiom_pointers.h"
+
+/**
+ *array_iterator - Executes a function given as a parametr
+ *@array: The provided array
+ *@size: Size of the array
+ *@action: Provided function
+ *
+ *Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i< size; i++)
+	{
+		action(array[i]);
+	}
+}
