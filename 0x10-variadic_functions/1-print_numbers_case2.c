bd9e2c229063e7ae6302713ee27377e608997274
Titus Denis <deetitos@gmail.com>
2023-08-18 01:34:16 +0100

    added 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers_case2.c
+++ 0x10-variadic_functions/1-print_numbers_case2.c
@@ -2,7 +2,28 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ *is_number - checks for numbers
+ *@str: passed string
+ *
+ *Return: A bool
+ */
+bool is_number(const char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+		{
+			return (false);
+		}
+		str++;
+	}
+	return (true);
+}
 /**
  *print_numbers - prints numberss passed as arguments
  *@separator: Passed argument
@@ -14,12 +35,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	const char *pstr;
 	int number;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		number = va_arg(args, int);
+		pstr = va_arg(args, const char*);
+		if (!is_number(pstr))
+			continue;
+		number = atoi(pstr);
 		printf("%d", number);
 		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
