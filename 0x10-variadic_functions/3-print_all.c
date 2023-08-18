a00aa6f0dd2e5efe9e13a5061c2ef027abc5ca4a
Titus Denis <deetitos@gmail.com>
2023-08-18 03:46:11 +0100

    commited

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,42 +11,38 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	const char *pformat = format;
-	char c_arg;
-	int i_arg;
-	float f_arg;
-	char *s_arg;
+	char c;
+	int i = 0;
+	float f;
+	char *s;
 
 	va_start(args, format);
-	while (pformat && *pformat)
+	while (format && format[i])
 	{
-		if (*pformat == 'c')
+		switch (format[i])
 		{
-			c_arg = va_arg(args, int);
-			printf("%c", c_arg);
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'f':
+				f = (float)  va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 's':
+				s = va_arg(args, char*);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
 		}
-		else if (*pformat == 'f')
-		{
-			f_arg = va_arg(args, double);
-			printf("%f", f_arg);
-		}
-		else if (*pformat == 'i')
-		{
-			i_arg = va_arg(args, int);
-			printf("%d", i_arg);
-		}
-		else if (*pformat == 's')
-		{
-			s_arg = va_arg(args, char *);
-			if (s_arg == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s_arg);
-		}
-		pformat++;
-		if (*pformat)
+		i++;
+		if (format[i])
 			printf(", ");
 	}
 	va_end(args);
-	putchar('\n');
+	printf("\n");
 }
