4b3044c9cbfb12e8d0c41a9df4527bf064a96795
Titus Denis <deetitos@gmail.com>
2023-08-29 14:04:24 +0100

    Commit 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *listint_len - Returns the number of nodes in a linked list
+ *
+ *Return: a value
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != 0)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
