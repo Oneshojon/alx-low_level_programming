c183be43203ad3feb4be1e0bd450db325948a2fe
Titus Denis <deetitos@gmail.com>
2023-10-27 04:00:13 +0100

    commit 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ *get_endianness - Checks for endianess
+ *
+ *Returns: 1 or 0
+ */
+ int get_endianness(void)
+{
+	int value = 1;
+	char *check = (char *)&value;
+
+	if (*check == 1)
+		return (1);
+	else
+		return (0);
+}
