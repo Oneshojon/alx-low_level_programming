6729d3b54c4cb201158a9c13411ba45f1d58f722
Titus Denis <deetitos@gmail.com>
2023-10-27 01:00:01 +0100

    commit 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int counts = 0;
 	unsigned int diff = n ^ m;
 
-	while (diff > 0)
+	while (diff != 0)
 	{
 		if (diff & 1)
 			counts += 1;
