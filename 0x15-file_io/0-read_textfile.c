231f184517a348e9e547110fe905d02c716a6ee3
Titus Denis <deetitos@gmail.com>
2023-10-30 12:53:38 +0100

    0-read_textfile.c Added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *read_textfile - Reads text file and print it to the PSIX
+ standard output
+ *@filename: pointer to the file
+ *@letter: Number of letter to read and write
+ *
+ *Return: The number of letter read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t bytesRead, bytesWritten;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytesRead = read(fd, buffer, letters);
+	if (bytesRead == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (bytesWritten);
+}
