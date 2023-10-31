516a54821f44bc27be5f0aa5cdb4d800a67ba6c5
Titus Denis <deetitos@gmail.com>
2023-11-01 00:13:11 +0100

    100-elf_header.c Added

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -68,11 +68,11 @@ void print_magic(unsigned char *e_ident)
 	while (index < EI_NIDENT)
 	{
 		printf("%02x", e_ident[index]);
-		index++;
 		if (index == EI_NIDENT - 1)
 			printf("\n");
 		else
-			print(" ");
+			printf(" ");
+		index++;
 	}
 }
 
@@ -243,4 +243,52 @@ void close_elf(int elf)
 		exit(98);
 	}
 }
+/**
+ *main - Displays ELF header information at the start of an ELF file.
+ *@argc: Count of arguments
+ *@argv: An array of pointers to the arguments
+ *
+ *Return: 0 if succes
+ */
+int main(int argc, char *argv[])
+{
+	Elf64_Ehdr *header;
+	int o, r;
+	(void)argc;
 
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
+
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
+	free(header);
+	close_elf(o);
+	return (0);
+}
